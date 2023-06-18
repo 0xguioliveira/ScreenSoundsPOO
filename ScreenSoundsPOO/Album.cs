d231ad1b65c6a7cbbdc06f3040d81fbf6a8c50ba
guilh <guilherme.852@hotmail.com>
2023-06-18 18:06:40 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSoundsPOO/Album.cs
@@ -0,0 +1,29 @@
+﻿class Album
+{
+
+    public Album(string nome)
+    {
+        Nome = nome;
+    }
+    public string Nome { get; }
+
+    public int DuracaoTotal => musicas.Sum(m => m.Duracao); // para cada música, Some a duração.
+
+    private List<Musica> musicas = new List<Musica>();
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+
+    public void ExibirMusicasDoAlbum()
+    {
+        Console.WriteLine($"Lista de músicas do álbum {Nome}\n");
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine($"Música: {musica.Nome}");
+        }
+        Console.WriteLine($"Esse álbum possui a duração de {DuracaoTotal}");
+    }
+    
+}
---
+++ ScreenSoundsPOO/Banda.cs
@@ -0,0 +1,25 @@
+﻿class Banda
+{
+    private List<Album> albuns= new List<Album>();
+
+    public Banda(string nome)
+    {
+        Nome = nome;
+    }
+
+    public string Nome { get; }
+
+    public void AdicionarAlbum(Album album)
+    {
+        albuns.Add(album);
+
+    }
+    public void ExibindoDiscografia()
+    {
+        Console.WriteLine($"\nExibindo discografia da banda {Nome}");
+        foreach(Album album in albuns)
+        {
+            Console.WriteLine($"Álbum: {album.Nome}, duração: {album.DuracaoTotal}");
+        }
+    }
+}
